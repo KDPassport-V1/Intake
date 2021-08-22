4eb554b4414777864b696551312a508b4387ea6a
IamTechDevil <iamtechdevil@gmail.com>
2021-08-22 17:51:17 +0530

    server change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
-  public static API_PATH = 'https://kdpassportv1prod.azurewebsites.net/kdapp/api/v1';
+  public static API_PATH = 'https://kdpassportv1prod.azurewebsites.net/kdapp';
   public static getLocations = '/registration/getOrgAndProjects';
   public static saveQuestionaire = "/registration/intakeUserCreate";
-  public static getCountryAndStates = '/master/getCountryAndStates';
+  public static getCountryAndStates = '/registration/getCountryAndStates';
 }
